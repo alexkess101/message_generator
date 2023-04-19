d28930626e5d2a3ac6f1f8b21ae7756144ee8569
alexkessinger <alex@smarty.com>
2023-04-19 13:49:52 -0600

    added two more messages

---
+++ salesOrderItemCancelled/main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+	"reflect"
+	"time"
+
+	"root/utils"
+
+	"bitbucket.org/smartystreets/message-registry/v2/meta"
+	"bitbucket.org/smartystreets/message-registry/v2/sales"
+)
+
+var Version string
+
+func main() {
+	now := time.Now()
+	message := sales.OrderItemCancelled{
+		Metadata:               meta.Meta{Timestamp: now},
+		AccountID:              0,
+		OrderID:                0,
+		OrderItemID:            0,
+		PlanID:                 0,
+		ReplacementOrderID:     0,
+		ReplacementOrderItemID: 0,
+		ReplacementPlanID:      0,
+		Reason:                 "test",
+		CompensationPlan:       0,
+	}
+
+	args := utils.GetArguments(Version)
+	messageValue := reflect.ValueOf(&message).Elem()
+
+	for i := 0; i < len(args); i += 2 {
+		utils.ApplyValueToMessage(args[i], args[i+1], messageValue)
+	}
+
+	orderJSON, err := json.Marshal(message)
+	if err != nil {
+		fmt.Println("Error:", err)
+		os.Exit(1)
+	}
+
+	err = utils.WriteClipboard(string(orderJSON))
+	if err != nil {
+		fmt.Println("Error:", err)
+		os.Exit(1)
+	}
+
+	utils.PrintSuccessMessage("sales:order-item-cancelled")
+}
---
+++ subscriptionSubscriptionCancelled/main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+	"reflect"
+	"time"
+
+	"root/utils"
+
+	"bitbucket.org/smartystreets/message-registry/v2/meta"
+	"bitbucket.org/smartystreets/message-registry/v2/subscription"
+)
+
+var Version string
+
+func main() {
+	now := time.Now()
+	message := subscription.SubscriptionCancelled{
+		Metadata:       meta.Meta{Timestamp: now},
+		SubscriptionID: 0,
+		OrderID:        0,
+		OrderItemID:    0,
+		LookupsIssued:  0,
+		LookupsUsed:    0,
+		SecondsIssued:  0,
+		SecondsUsed:    0,
+	}
+
+	args := utils.GetArguments(Version)
+	messageValue := reflect.ValueOf(&message).Elem()
+
+	for i := 0; i < len(args); i += 2 {
+		utils.ApplyValueToMessage(args[i], args[i+1], messageValue)
+	}
+
+	orderJSON, err := json.Marshal(message)
+	if err != nil {
+		fmt.Println("Error:", err)
+		os.Exit(1)
+	}
+
+	err = utils.WriteClipboard(string(orderJSON))
+	if err != nil {
+		fmt.Println("Error:", err)
+		os.Exit(1)
+	}
+
+	utils.PrintSuccessMessage("subscription:subscription-cancelled")
+}
